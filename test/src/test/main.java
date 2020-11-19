8d297ea2d4a9fd9c0c4b9cd161c368f41cd59fdf
Ghassenkh22 <74678407+Ghassenkh22@users.noreply.github.com>
2020-11-19 14:06:59 +0100

    Update main.java

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -15,7 +15,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
-		System.out.println("i did some changes");
+		System.out.println(" heyyy i did some changes");
 	}
 
 }
